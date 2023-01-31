a1267731057ab93809d6a5b0e05bc81038332c7d
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-01-31 16:03:47 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Website AFG',
-  tagline: 'Receba',
+  tagline: 'Bem Vindo',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
