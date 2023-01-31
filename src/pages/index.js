69662cdbfb1e2d668a81295e2d1ed28b700e07fc
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-01-31 17:20:33 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -11,8 +12,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="herotitle">{siteConfig.title}</h1>
-        <p className="herosubtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -29,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={Hello from ${siteConfig.title}}
+      title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
