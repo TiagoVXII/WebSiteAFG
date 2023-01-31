86e4a36c4f9a38c59b622e1a0cb8896ef656ae50
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-01-31 17:01:44 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Clique aqui para continuar ⏱️
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -29,7 +29,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={{siteConfig.title}}
+      title={Hello from ${siteConfig.title}}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
