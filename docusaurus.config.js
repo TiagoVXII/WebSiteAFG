c859bb24f28a4ce2921569eff96633b5c875d6aa
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-02-02 15:43:42 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'site/intro',
             position: 'left',
             label: 'Documentos',
           },
